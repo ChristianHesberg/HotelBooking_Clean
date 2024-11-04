4d97d4d0579433b0157dab29ddd0ec92e6d4b483
Victorkaffekande <victor.gugerel@gmail.com>
2024-11-04 23:37:09 +0100

    moved unused test cases

--- HotelBooking.Specs/Features/BookingManager.feature
+++ HotelBooking.Specs/Features/BookingManager.feature
@@ -12,25 +12,7 @@ Scenario: Create valid booking
 	
 Scenario: User inputs invalid start date 
 	Given the start date is yesterday
-	And the end date is tomorrow
+	And the end date is valid tomorrow
 	When I create a booking with these dates
 	Then the booking should reject and inform user to fix dates
 	
-Scenario: User inputs invalid end date 
-	Given the start date is today
-	And the end date is yesterday
-	When I create a booking with these dates
-	Then the booking should reject and inform user to fix dates
-	
-Scenario: User inputs end date before start date 
-	Given the start date is tomorrow
-	And the end date is today
-	When I create a booking with these dates
-	Then the booking should reject and inform user to fix dates
-	
-Scenario: User tries to book but no rooms are available 
-	Given the start date is today
-	And the end date is tomorrow
-	But no rooms are available
-	When I create a booking with these dates
-	Then the booking should reject and inform user there are no available rooms
---
+++ HotelBooking.Specs/StepDefinitions/InvalidStartDateStepDefinitions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HotelBooking.Core;
+using Moq;
+using Reqnroll;
+using Xunit;
+
+namespace HotelBooking.Specs.StepDefinitions;
+
+[Binding]
+public class InvalidStartDateStepDefinitions
+{
+    private DateTime _startDate;
+    private DateTime _endDate;
+    private Booking _booking;
+    private bool _bookingSuccess;
+
+    private Mock<IRepository<Room>> roomRepository = new();
+    private Mock<IRepository<Booking>> bookingRepository = new();
+    private BookingManager bookingManager;
+
+    public InvalidStartDateStepDefinitions()
+    {
+        bookingManager = new BookingManager(bookingRepository.Object, roomRepository.Object);
+    }
+
+    [Given(@"the start date is yesterday")]
+    public void GivenStartDateIsYesterday()
+    {
+        _startDate = DateTime.Now.AddDays(-1);
+    }
+
+    [Given(@"the end date is valid tomorrow")]
+    public void GivenTheEndDateIsTomorrow()
+    {
+        _endDate = DateTime.Now.AddDays(1);
+    }
+
+    private Action act;
+
+    [When(@"I create a booking with these wrong dates")]
+    public void WhenICreateABookingWithTheseDates()
+    {
+        _booking = new Booking()
+        {
+            Id = 1,
+            StartDate = _startDate,
+            EndDate = _endDate
+        };
+    }
+
+    [Then(@"the booking should reject and inform user to fix dates")]
+    public void ThenTheBookingShouldBeCreatedSuccessfully()
+    {
+        //this should work, but does not. 
+        Assert.Throws<ArgumentException>(() => bookingManager.CreateBooking(_booking));
+    }
+}
